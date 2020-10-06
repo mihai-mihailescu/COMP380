6ed4e864bb42da1d325bee301c23f974949666a0
Hicks, Brandon, Arvato SCM Solutions <Brandon.Hicks@arvato.com>
2020-10-06 08:32:07 -0700

    Add project files.

---
+++ Data/Resources/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Data.Resources
+{
+    public class Resource
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string AssignedTask { get; set; }
+    }
+}
---
+++ Data/Resources/ResourceService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Data.Resources
+{
+    public class ResourceService
+    {
+        private static readonly Resource[] SeedResources = new[]
+        {
+            new Resource() {Name = "Mihai", Title = "Architect", AssignedTask="Task1"},
+            new Resource() {Name = "Richard", Title = "Developer", AssignedTask="Task2"},
+            new Resource() {Name = "Michelle", Title = "QA Engineer", AssignedTask="Task3"},
+            new Resource() {Name = "Andranik", Title = "Business Analyst", AssignedTask="Task4"},
+            new Resource() {Name = "Brandon", Title = "Designer", AssignedTask="Task5"}
+        };
+
+        public Task<List<Resource>> GetResourcesAsync()
+        {
+            var resourceList = new List<Resource>();
+            foreach(var resource in SeedResources)
+            {
+                resourceList.Add(resource);
+            }
+
+            return Task.FromResult(resourceList);
+        }
+    }
+}
