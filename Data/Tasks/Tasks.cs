be3e96cbe5051201478911c323bafbd288d657d0
Hicks, Brandon, Arvato SCM Solutions <Brandon.Hicks@arvato.com>
2020-10-16 20:45:23 -0700

    Finished task edit view

--- Data/Tasks/Tasks.cs
+++ Data/Tasks/Tasks.cs
@@ -11,14 +11,14 @@ namespace ProjectManagementSystem.Data.Tasks
         public string ResourceAssigned { get; set; }
         public DateTime? ExpectedStartDate { get; set; }
         public DateTime? ExpectedEndDate { get; set; }
-        public int? ExpectedDuration { get; set; }
+        public string ExpectedDuration { get; set; }
         public int? ExpectedEffort { get; set; }
         public DateTime? ActualStartDate { get; set; }
         public DateTime? ActualEndDate { get; set; }
-        public int? ActualDuration { get; set; }
+        public string ActualDuration { get; set; }
         public int? EffortCompleted { get; set; }
         public int? ActualEffort { get; set; }
-        public decimal? PercentCompleted { get; set; }
+        public string PercentCompleted { get; set; }
         public List<string> PredecessorTasks { get; set; }
         public List<string> SuccessorTasks { get; set; }
         public List<string> ListOfIssues { get; set; }
