f3ab2cd42448ffe6e71d7fa6501c90d80ded1c2e
nomodfx <37909374+nomodfx@users.noreply.github.com>
2020-11-20 09:18:31 -0800

    Create IssueListModel.cs

---
+++ Features/Issues/Models/IssueListModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ProjectManagementSystem.Features.Issues.Models
+{
+    public class IssueListModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ActionItemName { get; set; }
+        public string DecisionName { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public Severity Severity { get; set; }
+        public DateTime DateRaised { get; set; }
+        public DateTime? DateAssigned { get; set; }
+        public DateTime? ExpectedCompletionDate { get; set; }
+        public DateTime? ActualCompletionDate { get; set; }
+        public Status Status { get; set; }
+        public string StatusDescription { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
