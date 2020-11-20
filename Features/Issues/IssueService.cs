fd7a0b16263ead367a06eb80d3a5187e6379feaa
nomodfx <37909374+nomodfx@users.noreply.github.com>
2020-11-20 09:30:33 -0800

    Update IssueService,cs for db

--- Features/Issues/IssueService.cs
+++ Features/Issues/IssueService.cs
@@ -13,6 +13,29 @@ namespace ProjectManagementSystem.Features.Issues
         {
             this.db = db;
         }
+        
+        public async Task<List<IssueListModel>> GetIssuesListDataAsync(){
+            var issueDataList = await(
+                from issue in this.db.Issue
+                //What to link here
+                select new IssueListModel
+                {
+                    Id = issue.Id,
+                    Name = issue.Name,
+                    Description = issue.Description,
+                    Priority = issue.Priority,
+                    Severity = issue.Severity,
+                    DateRaised = issue.DateRaised,
+                    DateAssigned = issue.DateAssigned,
+                    ExpectedCompletionDate = issue.ExpectedCompletionDate,
+                    ActualCompletionDate = issue.ActualCompletionDate, 
+                    Status = issue.Status,
+                    StatusDescription = issue.StatusDescription,
+                    UpdateDate = issue.UpdateDate
+                }
+            ).ToListAsync();
+            return issueDataList;
+        }
 
         public async Task<List<Issue>> GetIssuesDataAsync()
         {
