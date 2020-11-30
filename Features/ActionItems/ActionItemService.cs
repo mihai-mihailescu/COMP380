8079d53d83d5b81c471b394ca37be54a40ebde61
MChheung <52295715+CakePopKid@users.noreply.github.com>
2020-11-30 14:41:55 -0800

    update service

---
+++ Features/ActionItems/ActionItemService.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystem.Data;
+using ProjectManagementSystem.Features.Tasks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Features.Tasks
+{
+    public class ActionItemService
+    {
+        private ApplicationDbContext db;
+
+        public ActionItemService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<List<ActionItemListModel>> GetActionItemsListDataAsync()
+        {
+            //var taskDataList = await (
+            //    from task in this.db.Task
+            //    join addResource in this.db.Resource on task.ResourceId equals addResource.Id
+            //    into TaskResourceData
+            //    from resource in TaskResourceData.DefaultIfEmpty()
+            //    select new TaskListModel { TaskId = task.Id, TaskName = task.Name, ExpectedStartDate = task.ExpectedStartDate, ExpectedEndDate = task.ExpectedEndDate, TaskType = task.TaskType, ResourceName = resource.Name }
+            //        ).ToListAsync();
+
+            //return taskDataList;
+        }
+
+        public async Task<TaskModel> GetTaskById(Guid TaskId)
+        {
+            //var taskModel = await (
+            //    from task in this.db.Task
+            //    join addResource in this.db.Resource on task.ResourceId equals addResource.Id
+            //    into TaskResourceData
+            //    from resource in TaskResourceData.DefaultIfEmpty()
+            //    where task.Id == TaskId
+            //    select new TaskModel { Task = task, Resource = resource }
+            //        ).FirstOrDefaultAsync();
+
+            //return taskModel;
+        }
+
+        public async Task<List<Task>> GetTasksDataAsync()
+        {
+            //var taskModel = await (
+            //    from task in this.db.Task.Include(x => x.TaskIssue).ThenInclude(x => x.Issue)
+            //    join addResource in this.db.Resource on task.ResourceId equals addResource.Id
+            //    into TaskResourceData
+            //    from resource in TaskResourceData.DefaultIfEmpty()
+            //    select task
+            //        ).ToListAsync();
+
+            //return taskModel;
+        }
+
+        public int SaveTask(Task taskData)
+        {
+            int entriesSaved = 0;
+
+            var task = new Task()
+            {
+                Name = taskData.Name,
+                Description = taskData.Description,
+                TaskType = taskData.TaskType,
+                ExpectedStartDate = taskData.ExpectedStartDate,
+                ExpectedEndDate = taskData.ExpectedEndDate,
+                ExpectedDuration = taskData.ExpectedDuration,
+                ExpectedEffort = taskData.ExpectedEffort,
+                ActualStartDate = taskData.ActualStartDate,
+                ActualEndDate = taskData.ActualEndDate,
+                ActualDuration = taskData.ActualDuration,
+                EffortCompleted = taskData.EffortCompleted,
+                ActualEffort = taskData.ActualEffort,
+                PercentCompleted = taskData.PercentCompleted,
+                ParentSummaryTaskId = taskData.ParentSummaryTaskId,
+                TaskPredecessor = taskData.TaskPredecessor,
+                TaskSuccessor = taskData.TaskSuccessor,
+                ParentGroupTaskId = taskData.ParentGroupTaskId,
+                DeliverableId = taskData.DeliverableId,
+                ResourceId = taskData.ResourceId,
+                TaskIssue = taskData.TaskIssue,
+            };
+
+            try
+            {
+                this.db.Add(task);
+                entriesSaved = this.db.SaveChanges();
+            }
+            catch (SqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return entriesSaved;
+        }
+    }
+}
---
+++ Features/ActionItems/Model/ActionItemListModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Features.ActionItems.Models
+{
+    public class ActionItemListModel
+    {
+        public Guid ActionItemId { get; set; }
+        public string IssueName { get; set; }
+        public string ResourceName { get; set; }
+        public string ActionItemName { get; set; }
+        public string ActionItemDescription { get; set; }
+        public string ActionItemStatus { get; set; }
+        public string ActionItemStatusDescription { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateAssigned { get; set; }
+        public
+
+    }
+}
+
